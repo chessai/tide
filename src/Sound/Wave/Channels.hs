0699bd32f251621dfb1b8852ec2907a199ea3d8a
Justin Prindle <justindprindle@gmail.com>
2019-08-12 22:01:01 -0400

    Add Monaural channel type for forcing boxed arrays

--- src/Sound/Wave/Channels.hs
+++ src/Sound/Wave/Channels.hs
@@ -1,5 +1,6 @@
 {-# language DeriveGeneric #-}
 {-# language DerivingStrategies #-}
+{-# language GeneralizedNewtypeDeriving #-}
 {-# language ScopedTypeVariables #-}
 {-# language TypeApplications #-}
 {-# language TypeFamilies #-}
@@ -7,6 +8,7 @@
 -- | Types for dealing with the channels in audio samples
 module Sound.Wave.Channels
   ( Stereo(..)
+  , Monaural(..)
   ) where
 
 import GHC.Generics
@@ -29,4 +31,17 @@ instance forall a. WaveSample a => WaveSample (Stereo a) where
   bytesPerChannel = bytesPerChannel @a
   getSample = Stereo <$> getSample <*> getSample
   putSample (Stereo c1 c2) = putSample c1 <> putSample c2
+
+-- | A single channel of audio. This will be represented with a boxed array, and
+-- can be used to force basic types to have a boxed representation.
+newtype Monaural a = Monaural { getMonaural :: a }
+  deriving newtype (Eq, Ord, Show)
+  deriving stock (Generic)
+
+instance forall a. WaveSample a => WaveSample (Monaural a) where
+  type SampleArr (Monaural a) = Array
+  numChannels = numChannels @a
+  bytesPerChannel = bytesPerChannel @a
+  getSample = Monaural <$> getSample
+  putSample (Monaural a) = putSample a
 
